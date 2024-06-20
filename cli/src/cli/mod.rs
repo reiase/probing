2f2dea8a6c898f157c292c42941faadac133fe09
Reiase <reiase@gmail.com>
2024-06-20 20:13:22 +0800

    refactor cli

--- cli/src/cli/mod.rs
+++ cli/src/cli/mod.rs
@@ -1,17 +1,16 @@
+use anyhow::Context;
+use anyhow::Result;
+use clap::Parser;
+use nix::{sys::signal, unistd::Pid};
+
 pub mod catch;
 pub mod commands;
 pub mod debug;
 pub mod inject;
 pub mod performance;
 
-use commands::Commands;
-
 use crate::inject::{Injector, Process};
-use anyhow::Context;
-use anyhow::Result;
-use nix::{sys::signal, unistd::Pid};
-
-use clap::Parser;
+use commands::Commands;
 
 /// Probe CLI - A performance and stability diagnostic tool for AI applications
 #[derive(Parser)]
@@ -21,8 +20,12 @@ pub struct Cli {
     dll: Option<std::path::PathBuf>,
 
     /// target process ID (e.g., 1234)
-    #[arg()]
-    pub pid: i32,
+    #[arg(short, long, conflicts_with_all=["name"])]
+    pub pid: Option<i32>,
+
+    /// target process name (e.g., "chrome.exe")
+    #[arg(short, long, conflicts_with_all=["pid"])]
+    pub name: Option<String>,
 
     #[command(subcommand)]
     command: Option<Commands>,
@@ -30,12 +33,26 @@ pub struct Cli {
 
 impl Cli {
     pub fn run(&self) -> Result<()> {
+        let pid = {
+            if let Some(pid) = self.pid {
+                pid
+            } else if let Some(name) = self.name.as_ref() {
+                let process = Process::by_name(name.as_str())
+                    .map_err(|err| {
+                        anyhow::anyhow!("failed to find process with name {}: {}", name, err)
+                    })?
+                    .unwrap();
+                process.pid()
+            } else {
+                return Err(anyhow::anyhow!("either `pid` or `name` must be specified"));
+            }
+        };
         match &self.command {
-            Some(Commands::Inject(cmd)) => cmd.run(self.pid, &self.dll),
-            Some(Commands::Debug(cmd)) => cmd.run(self.pid),
-            Some(Commands::Performance(cmd)) => cmd.run(self.pid),
+            Some(Commands::Inject(cmd)) => cmd.run(pid, &self.dll),
+            Some(Commands::Debug(cmd)) => cmd.run(pid),
+            Some(Commands::Performance(cmd)) => cmd.run(pid),
             // Some(Commands::CatchCrash(cmd)) => cmd.run(self.pid),
-            None => inject::InjectCommand::default().run(self.pid, &self.dll),
+            None => inject::InjectCommand::default().run(pid, &self.dll),
         }
     }
 }
