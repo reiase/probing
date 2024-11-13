c9f861876e20514369ac50bf00b1c990cc174b3e
Reiase <reiase@gmail.com>
2024-11-13 14:12:21 +0800

    add table plugin

--- engine/src/plugins/mod.rs
+++ engine/src/plugins/mod.rs
@@ -1,2 +1,3 @@
 pub mod env;
+pub mod env2;
 pub mod files;
--- examples/query.rs
+++ examples/query.rs
@@ -7,12 +7,16 @@ use engine::core::Engine;
 use engine::plugins::env::EnvPlugin;
 use engine::plugins::files::FilesPlugin;
 
+use engine::plugins::env2::EnvPlugin2;
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let engine = Engine::new();
 
     engine.enable("probe".to_string(), Arc::new(EnvPlugin::default()))?;
     engine.enable("probe".to_string(), Arc::new(FilesPlugin::default()))?;
+    engine.enable("probe".to_string(), Arc::new(EnvPlugin2::new("envs2".to_string(), "process".to_string())))?;
+
 
     let query = std::env::args().collect::<Vec<_>>()[1].clone();
     let rb = engine.sql(query.as_str()).await?.collect().await?;
