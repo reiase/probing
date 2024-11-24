09426b2500832e7c7f856c709c9c18b3298b86ab
Reiase <reiase@gmail.com>
2024-11-24 23:30:12 +0800

    add python plugin

--- engine/src/core/mod.rs
+++ engine/src/core/mod.rs
@@ -5,9 +5,18 @@ pub use engine::Engine;
 pub use engine::Plugin;
 pub use engine::PluginType;
 
-pub use table_plugin::CustomTable;
 pub use table_plugin::CustomSchema;
-pub use table_plugin::TablePlugin;
+pub use table_plugin::CustomTable;
 pub use table_plugin::SchemaPlugin;
+pub use table_plugin::TablePlugin;
 
+pub use datafusion::arrow::array::ArrayRef;
+pub use datafusion::arrow::array::Float64Array;
+pub use datafusion::arrow::array::Int64Array;
+pub use datafusion::arrow::array::RecordBatch;
+pub use datafusion::arrow::array::StringArray;
+pub use datafusion::arrow::datatypes::DataType;
+pub use datafusion::arrow::datatypes::Field;
+pub use datafusion::arrow::datatypes::Schema;
+pub use datafusion::arrow::datatypes::SchemaRef;
 pub use datafusion::arrow::util::pretty;
