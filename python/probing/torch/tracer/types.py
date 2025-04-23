2f45752eafda9ac757753925df61ab24f6c2e77c
reiase <reiase@gmail.com>
2025-04-23 12:43:24 +0800

    feat: add TensorDef and BaseTracer classes for tracing hooks in PyTorch

---
+++ python/probing/torch/tracer/types.py
@@ -0,0 +1,32 @@
+from typing import Any, List
+from dataclasses import dataclass
+
+
+@dataclass
+class TensorDef:
+    shape: tuple = ()
+    dtype: Any = None
+
+    @staticmethod
+    def create(t):
+        return TensorDef(t.shape, t.dtype)
+
+
+class BaseTracer:
+    def pre_forward_hook(self, m, i):
+        pass
+
+    def post_forward_hook(self, m, i, o):
+        pass
+
+    def pre_backward_hook(self, m, i):
+        pass
+
+    def post_backward_hook(self, m, i, o):
+        pass
+
+    def pre_step_hook(self, optimizer, args, kwargs):
+        pass
+
+    def post_step_hook(self, optimizer, args, kwargs):
+        pass
