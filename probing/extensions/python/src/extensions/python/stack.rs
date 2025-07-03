f89e559d4e2816b046072b31ecf294458c999284
yangrudan <1549029765@qq.com>
2025-07-03 14:57:15 +0800

    feat: add local variable conversion to map in Python stack tracing(haven't captured)

--- probing/extensions/python/src/extensions/python/stack.rs
+++ probing/extensions/python/src/extensions/python/stack.rs
@@ -1,7 +1,7 @@
-use probing_proto::prelude::CallFrame;
+use probing_proto::prelude::{CallFrame, Value};
+use std::collections::HashMap;
+use py_spy::stack_trace::LocalVariable;
 
-
-const STACK_THREADS: &str = include_str!("stack_get_threads.py");
 
 pub fn get_python_stacks(pid: py_spy::Pid) -> Option<Vec<CallFrame>> {
     let mut frames = vec![];
@@ -17,10 +17,10 @@ pub fn get_python_stacks(pid: py_spy::Pid) -> Option<Vec<CallFrame>> {
         Err(e) => {
             log::error!("Failed to get stack traces: {}", e);
             return None;
-    }
-};
+        }
+    };
 
-    // Print out the python stack for each thread
+    // figure out the python stack for each thread
     for trace in traces {
         log::debug!("!!!!Thread {:#X} ({})", trace.thread_id, trace.status_str());
         for frame in &trace.frames {
@@ -29,10 +29,32 @@ pub fn get_python_stacks(pid: py_spy::Pid) -> Option<Vec<CallFrame>> {
                 file: frame.filename.clone(),
                 func: frame.name.clone(),
                 lineno: frame.line as i64,
-                locals: Default::default(),
+                locals: convert_locals_to_map(frame.locals.clone()),
             });
         }
     }
 
     Some(frames)
+}
+
+pub fn convert_locals_to_map(locals: Option<Vec<LocalVariable>>) -> HashMap<String, Value> {
+    log::debug!("Converting locals to map...{:#?}", locals);
+    let mut map = HashMap::new();
+    
+    if let Some(local_vars) = locals {
+        for var in local_vars {
+            let value = Value {
+                id: var.addr as u64,
+                class: "Variable".to_string(),
+                shape: None, 
+                dtype: None, 
+                device: None, 
+                value: var.repr,
+            };
+            
+            map.insert(var.name, value);
+        }
+    }
+    log::debug!("Converted locals to map: {:?}", map);
+    map
 }
